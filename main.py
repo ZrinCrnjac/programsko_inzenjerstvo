ccd4bb3eb6be93a20611ef5049fad36b4b4870c1
Crnjac <zrin.crnjac@gmail.com>
2017-11-17 16:41:14 +0100

    Move parseing to course.py

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 import random
 
 from classes.course import Course
+from classes.course import get_courses_from_file
 from classes.student import Student
-
-def get_courses_from_file(filename):
-    with open(filename, "r") as f:
-        lines = f.readlines()
-
-    data = []
-    for a in lines:
-        if not a.startswith("#"):
-            data.append(a.strip())
-
-    courses = []
-
-    for line in data:
-        x = line.split(" | ")
-        if "Course" in x[0]:
-            courses.append(Course(x[1], x[2]))
-        elif "Running" in x[0]:
-            courses[-1].add_running(int(x[1]))
-    return courses
-
 
 if __name__ == "__main__":
     courses = get_courses_from_file("courses.txt")
