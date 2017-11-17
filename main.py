59eda0b85d62bf94f43594702f31f8584c2fc93e
Crnjac <zrin.crnjac@gmail.com>
2017-11-17 16:21:44 +0100

    Changed output way

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import random
 from classes.course import Course
 from classes.student import Student
 
-pi = Course("Programsko inzenjerstvo", "SARS503")
-pi2017 = pi.add_running(2017)
-pi2016 = pi.add_running(2016)
-print pi
-mat = Course("Matematika 1", "SER101")
-mat.add_running(2017)
-mat.add_running(2016)
-print mat
+with open("courses.txt", "r") as f:
+    lines = f.readlines()
+
+data = []
+for a in lines:
+    if not a.startswith("#"):
+        data.append(a.strip())
+
+courses = []
+
+for line in data:
+    x = line.split(" | ")
+    if "Course" in x[0]:
+        courses.append(Course(x[1], x[2]))
+    elif "Running" in x[0]:
+        courses[-1].add_running(int(x[1]))
+
+for nesto in courses:
+    print nesto
